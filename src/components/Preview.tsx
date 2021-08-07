cc6edb259a7786e2297b303ee11b6d53c731ae04
flyinglimao <hi@limaois.me>
2021-08-07 23:22:28 +0800

    fix: update default project

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import $ from 'jquery'
 import { ProjectContext } from '../contexts/ProjectContext'
 import { AppAction, AppContext } from '../contexts/AppContext'
+import { svg } from '../contexts/defaultData.json'
 
 const Root = styled.div`
   width: calc(100% - 484px);
@@ -32,13 +33,7 @@ export default function Preview (): React.ReactElement {
   const { dispatchAppState } = useContext(AppContext)!
 
   // TODO: Replace with placeholder image
-  const [svgContent, setSvgContent] = useState(`
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="100" height="100">
-      <path d="M30,1h40l29,29v40l-29,29h-40l-29-29v-40z" stroke="#000" fill="none"/>
-      <path d="M31,3h38l28,28v38l-28,28h-38l-28-28v-38z" fill="#a23"/>
-      <text x="50" y="68" font-size="48" fill="#FFF" text-anchor="middle"><![CDATA[410]]></text>
-    </svg>
-  `)
+  const [svgContent, setSvgContent] = useState(svg)
   useEffect(() => {
     if (!projectState.svg) return // TODO: Replace preview with error message image
 
