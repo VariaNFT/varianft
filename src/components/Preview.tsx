ad451876498e27295ad7ff9d545b495ddd8eb64b
flyinglimao <hi@limaois.me>
2021-08-08 17:55:10 +0800

    fix: remote img loading error handling

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -4,6 +4,7 @@ import $ from 'jquery'
 import { ProjectContext } from '../contexts/ProjectContext'
 import { AppAction, AppContext } from '../contexts/AppContext'
 import { svg } from '../contexts/defaultData.json'
+import { loadImageAsDataUrl } from '../utils/loadImageAsDataUrl'
 
 const Root = styled.div`
   width: calc(100% - 484px);
@@ -35,58 +36,83 @@ export default function Preview (): React.ReactElement {
   // TODO: Replace with placeholder image
   const [svgContent, setSvgContent] = useState(svg)
   useEffect(() => {
-    if (!projectState.svg) return // TODO: Replace preview with error message image
+    (async () => {
+      if (!projectState.svg) return // TODO: Replace preview with error message image
 
-    const data = projectState.data[projectState.usingData]
-    let $svgElement
-    if (!data) {
-      setSvgContent(projectState.svg)
-    } else {
-      // Replace text
-      let copy = projectState.svg
-      Object.entries(data).forEach(([key, value]) => {
-        copy = copy.replaceAll(`{${key}}`, value)
-      })
-
-      // Replace Image
-      const $svg = $($.parseXML(copy))
-      if ($svg) {
-        $svg.find('image').each((_, element) => {
-          if (element.id && data[element.id]) {
-            const $element = $(element)
-            $element.attr('xlink:href', '')
-            $element.attr('href', data[element.id])
-          }
-        })
-        $svgElement = $svg.find('svg')
-
-        // Firefox Fix for SVGs without height and width, use last 2 value of view box
-        if ((!$svgElement.attr('width') || !$svgElement.attr('height')) && $svgElement.attr('viewBox')) {
-          const { width, height } = $svgElement.attr('viewBox')?.match(/\d+ \d+ (?<width>\d+) (?<height>\d+)/)?.groups || { width: 0, height: 0 }
-          $svgElement.attr('width', width)
-          $svgElement.attr('height', height)
-        }
-        setSvgContent($svgElement[0].outerHTML)
+      const data = projectState.data[projectState.usingData]
+      let $svgElement
+      if (!data) {
+        setSvgContent(projectState.svg)
       } else {
-        // Svg error
-      }
-    }
+        // Replace text
+        let copy = projectState.svg
+        Object.entries(data).forEach(([key, value]) => {
+          copy = copy.replaceAll(`{${key}}`, value)
+        })
 
-    // Update preview image
-    if (!previewer.current) return
-    const svgDataURL = 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent($svgElement?.[0].outerHTML.trim() || svgContent.trim())
-    const img = new Image()
-    img.onload = () => {
-      const context = canvas.current!.getContext('2d')
-      canvas.current!.width = img.width
-      canvas.current!.height = img.height
-      context?.drawImage(img, 0, 0)
-      setProjectState(prev => ({
-        ...prev,
-        preview: canvas.current!.toDataURL()
-      }))
-    }
-    img.src = svgDataURL
+        // Replace Image
+        const $svg = $($.parseXML(copy))
+        if ($svg) {
+          const loadImages = $svg.find('image').map((_, element) => {
+            if (element.id && data[element.id]) {
+              if (data[element.id].startsWith('http')) {
+                return loadImageAsDataUrl(data[element.id], { width: +(element.getAttribute('width') || 50), height: +(element.getAttribute('height') || 50) })
+                  .then(dataURL => {
+                    const $element = $(element)
+                    $element.attr('xlink:href', '')
+                    $element.attr('href', dataURL)
+                  })
+                  .catch(dataURL => {
+                    const $element = $(element)
+                    $element.attr('xlink:href', '')
+                    $element.attr('href', dataURL)
+                    dispatchAppState({
+                      action: AppAction.PUSH_TOAST,
+                      payload: {
+                        color: 'error',
+                        message: 'Unable to get the image, check out the document "Troubleshooting > Unable to Get the Image"',
+                      }
+                    })
+                  })
+              } else {
+                const $element = $(element)
+                $element.attr('xlink:href', '')
+                $element.attr('href', data[element.id])
+              }
+            }
+            return Promise.resolve(null)
+          })
+          await Promise.all(loadImages)
+          $svgElement = $svg.find('svg')
+
+          // Firefox Fix for SVGs without height and width, use last 2 value of view box
+          if ((!$svgElement.attr('width') || !$svgElement.attr('height')) && $svgElement.attr('viewBox')) {
+            const { width, height } = $svgElement.attr('viewBox')?.match(/\d+ \d+ (?<width>\d+) (?<height>\d+)/)?.groups || { width: 0, height: 0 }
+            $svgElement.attr('width', width)
+            $svgElement.attr('height', height)
+          }
+          setSvgContent($svgElement[0].outerHTML)
+        } else {
+          // TODO: Svg error
+        }
+      }
+
+      // Update preview image
+      if (!previewer.current) return
+      const svgDataURL = 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent($svgElement?.[0].outerHTML.trim() || svgContent.trim())
+      const img = new Image()
+      img.onload = () => {
+        const context = canvas.current!.getContext('2d')
+        canvas.current!.width = img.width
+        canvas.current!.height = img.height
+        context?.drawImage(img, 0, 0)
+        setProjectState(prev => ({
+          ...prev,
+          preview: canvas.current!.toDataURL()
+        }))
+      }
+      img.src = svgDataURL
+    })()
   }, [projectState.usingData, projectState.data, projectState.svg])
 
   useEffect(() => {
---
+++ src/utils/loadImageAsDataUrl.ts
@@ -0,0 +1,29 @@
+export async function loadImageAsDataUrl (src: string, defaultSize: {width: number, height: number}): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
+    const img = new Image()
+
+    img.crossOrigin = 'anonymous'
+    img.onload = () => {
+      canvas.width = img.width
+      canvas.height = img.height
+      ctx?.drawImage(img, 0, 0)
+      resolve(canvas.toDataURL())
+      canvas.remove()
+    }
+    img.onerror = () => {
+      canvas.width = defaultSize.width
+      canvas.height = defaultSize.height
+      ctx!.fillStyle = '#ccc'
+      ctx?.fillRect(0, 0, defaultSize.width, defaultSize.height)
+      ctx!.fillStyle = '#000'
+      ctx!.textAlign = 'center'
+      ctx!.font = '8px Arial'
+      ctx?.fillText('Unable to get the image', defaultSize.width / 2, defaultSize.height / 2, defaultSize.width * 0.9)
+      reject(canvas.toDataURL())
+      canvas.remove()
+    }
+    img.src = src
+  })
+}
